c40637e193396d6008afe09a78c27dbe95843567
JesusAParedes <paredesjesus93@gmail.com>
2022-10-30 17:22:39 -0500

    added a comment to line 63

--- dom-tictactoe.js
+++ dom-tictactoe.js
@@ -60,7 +60,7 @@ const checkForWin = () => {
   if(horizontalWin() || verticalWin() || diagonalWin()) {
     // **BONUS** you could make the dismissal of this alert window reset the board...
     window.alert(`Congrats ${names} you won with ${currentMarker}!`)
-    resetBoard()
+    resetBoard() // just add the function to reset the board with the alert
   } else {
     // if no win, change the marker from X to O, or O to X for the next player.
     changeMarker()
